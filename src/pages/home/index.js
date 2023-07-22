c3a80a29daf49df113b311feef6d1018d6192976
Elias Treadway <45334183+McDiamund@users.noreply.github.com>
2023-07-21 23:55:43 -0400

    Landing Page Base Completed

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,12 @@ import styles from './index.module.css';
 import downArrow from 'assets/down-arrow.svg';
 import realtorLogo from 'assets/realtor-logo.png';
 import equalHousing from 'assets/equal-housing.webp';
+import surplusAsset from 'assets/surplus.png';
+import JR from 'assets/JR.png';
+import RT from 'assets/RT.jpg';
+import DG from 'assets/DG.jpg';
+import AE from 'assets/AE.jpg';
+import ET from 'assets/1649097022047.jfif';
 
 function Home() {
 
@@ -106,6 +112,178 @@ function Home() {
                     <div className={styles.tooltip}>
                         <p>HOVER TO SEE</p>
                     </div>
+                </div>
+
+            </div>
+
+            
+            <div className={styles.teamSection}>
+
+                <div className={styles.container}>
+
+                    <div>
+
+                        <div className={styles.teamHeader}>
+                            <p>Meet the Team</p>
+                            <div className={styles.teamAccentBar}></div>
+                        </div>
+
+                        <div className={styles.row}>
+
+                            <div className={styles.teamCol}>
+                                
+                                <div className={styles.teamImage} style={{
+                                    background: `url(${JR})`,
+                                    backgroundSize: "cover"
+                                }}></div>
+
+                                <p className={styles.name}>Jacqueline R.</p>
+                                <p className={styles.title}>Prinicipal Broker</p>
+
+                            </div>
+
+                            <div className={styles.teamCol}>
+                                
+                                <div className={styles.teamImage} style={{
+                                    background: `url(${RT})`,
+                                    backgroundSize: "cover"
+                                }}></div>
+
+                                <p className={styles.name}>Raymond T.</p>
+                                <p className={styles.title}>Credit Counselor & <br/> Down Payment Specialist</p>
+
+                            </div>
+
+                            <div className={styles.teamCol}>
+                                
+                                <div className={styles.teamImage} style={{
+                                    background: `url(${DG})`,
+                                    backgroundSize: "cover"
+                                }}></div>
+
+                                <p className={styles.name}>Devany G.</p>
+                                <p className={styles.title}>Luxury Homes & <br/> International Relocation</p>
+
+                            </div>
+
+                            <div className={styles.teamCol}>
+                                
+                                <div className={styles.teamImage} style={{
+                                    background: `url(${AE})`,
+                                    backgroundSize: "cover"
+                                }}></div>
+
+                                <p className={styles.name}>Alisa E.</p>
+                                <p className={styles.title}>Customer Service & <br/> Events Coordinator</p>
+
+                            </div>
+
+                            <div className={styles.teamCol}>
+                                
+                                <div className={styles.teamImage} style={{
+                                    background: `url(${ET})`,
+                                    backgroundSize: "cover"
+                                }}></div>
+
+                                <p className={styles.name}>Elias T.</p>
+                                <p className={styles.title}>Web Designer/Developer & <br/> IT Specialist</p>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div className={styles.surplusSection}>
+
+                <div className={styles.container}>
+
+                    <div className={styles.heroText}>
+
+                        <img src={surplusAsset} />
+
+                    </div>
+
+                    <div className={styles.details}>
+
+                        <p>You may be entitled to surplus funds.</p>
+
+                        <p>Have you lost your home in a foreclosure? Any excess funds acquired from <br/>
+                        a tax or mortgage foreclosure sale on your home are legally entitled to you. <br/> <br/>
+                        
+                        Depending on the laws of the state, the previous property owner has anywhere from <br/>
+                        a few months to a few years to claim surplus funds.</p>
+
+                        <div className={styles.contactButton}>
+                            <p>LEARN MORE</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className={styles.contactSection}>
+
+                <div className={styles.contactMap}>
+
+                    <div className={styles.map}>
+
+                    </div>
+
+                    <div className={styles.footer}>
+
+                        <p>Superior Advantage Realtors Inc. © 2023</p>
+
+                        <p>478-862-9533</p>
+
+                    </div>
+                </div>
+
+                <div className={styles.contactForm}>
+
+                    <div className={styles.container}>
+
+                        <p className={styles.contactHeader}>CONTACT US</p>
+
+                        <p className={styles.contactInfo}>Contact us any time you need professional support or have any questions. </p>
+
+                        <form>
+
+                            <div style={{ display: "flex", width: "100%"}}>
+
+                                <div className={styles.formInput} style={{ width: "50%"}}>
+                                    <label>Name</label>
+                                    <input placeholder='Name' type='text' />
+                                </div>
+
+                                <div className={styles.formInput} style={{ width: "50%"}}>
+                                    <label>Email</label>
+                                    <input placeholder='Email' type='text' />
+                                </div>
+
+                            </div>
+
+                            <div className={styles.formInput}>
+                                <label>Message</label>
+                                <textarea placeholder='Type something here...' rows={10}></textarea>
+
+                                <button className={styles.contactSubmit} type="submit">SEND</button>
+                            </div>
+
+
+
+                        </form>
+
+
+                    </div>
+
                 </div>
 
             </div>
